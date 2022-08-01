e591f590b141160e23ead2aed2bd561200b913ba
tuanhaaa <88657676+tuanhaaa@users.noreply.github.com>
2022-08-01 14:39:58 +0700

    upload source code

---
+++ app.js
@@ -0,0 +1,50 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let hitPosition
+let currentTime = 10
+
+function randomSquare() {
+  squares.forEach(square => {
+    square.classList.remove('mole')
+  })
+
+  let randomSquare = squares[Math.floor(Math.random() * 9)]
+  randomSquare.classList.add('mole')
+
+  hitPosition = randomSquare.id
+}
+
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+function countDown(){
+    currentTime--
+    timeLeft.textContent = currentTime
+    if (currentTime == -1){
+        clearInterval(countDownTimerId)
+        alert('GAME OVER! Your final score is ' + result)
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 2000)
